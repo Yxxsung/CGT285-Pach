ea29bf10d6d5eb025b27faab84a88189325ba118
Sophia Alexander <sophia.alexander@aol.com>
2026-03-13 02:10:56 -0400

    Final Upload - Assignment Complete

--- main.py
+++ main.py
@@ -22,6 +22,16 @@ line1Body = pymunk.Body(body_type=pymunk.Body.STATIC)
 line1 = pymunk.Segment(line1Body, (200, 200), (300, 300), 2)
 space.add(line1,line1Body)
 
+#This Creates The Second Ramp
+line2Body = pymunk.Body(body_type=pymunk.Body.STATIC)
+line2 = pymunk.Segment(line2Body, (450, 250), (300, 400), 2)
+space.add(line2,line2Body)
+
+#This Creates The Third Ramp
+line3Body = pymunk.Body(body_type=pymunk.Body.STATIC)
+line3 = pymunk.Segment(line3Body, (200, 450), (300, 550), 2)
+space.add(line3,line3Body)
+
 #This is the game loop
 done = False
 while not done:
@@ -35,6 +45,8 @@ while not done:
     pygame.draw.circle(screen, (255, 255, 255), body.position, 10)
     # this draws the ramp
     pygame.draw.line(screen,(255, 255, 255),line1.a,line1.b,2)
+    pygame.draw.line(screen, (255, 255, 255), line2.a, line2.b, 2)
+    pygame.draw.line(screen, (255, 255, 255), line3.a, line3.b, 2)
     pygame.display.update()
 
     #Now my objective is to add two more ramps. From here down is my own code
